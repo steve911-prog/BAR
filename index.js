9c3fdb3974d77f49a45add9f2e6ed7b89e97a928
steve911-prog <bwlad0911@gmail.com>
2023-06-19 17:57:58 +0200

    Updare

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let budget = 199;                                                                     //Client's money
-let yours = document.getElementById("yourMoney");
+import goods from "./goods.json" assert { type: "json" };
+try {
+  console.log(goods["coke"]);
+} catch (error) {
+  console.log("error json");
+}
 
+let budget = 199; //Client's money
+let yours = document.getElementById("yourMoney");
 
 let cokePrice = document.getElementsByClassName("cokePrice").textContent;
 let fantaPrice = document.getElementsByClassName("fantaPrice").textContent;
@@ -10,7 +16,7 @@ let buyCoke = document.getElementsByClassName("buyCoke");
 let buyFanta = document.getElementsByClassName("buyFanta");
 let buyWater = document.getElementsByClassName("buyWater");
 
-document.getElementById("cart").textContent = "0"
+document.getElementById("cart").textContent = "0";
 
 let prices = {
   coke: 20,
@@ -21,51 +27,46 @@ let prices = {
   xset: 59,
   mojito: 9,
   fruitjuices: 16,
-  lemonade: 9
+  lemonade: 9,
 };
 
-
-
-  alert("You got bonus 199$. Enjoy them!");
+alert("You got bonus 199$. Enjoy them!");
 
 let renewBucks = () => {
   document.getElementById("yourMoney").textContent = `${budget}$`;
-  console.log("renwBucks")
-
+  // console.log("renwBucks")
 };
 
 function renewDrink(num, id) {
   document.querySelector(`.${id}`).textContent = `${num}$`;
-  console.log("renwDrink!")
-  console.log(id)
-  console.log(document.getElementsByClassName(`${id}`).textContent);
-
-};
+  // console.log("renwDrink!")
+  // console.log(id)
+  // console.log(document.getElementsByClassName(`${id}`).textContent);
+}
 
 function request(drink) {
   if (budget <= 0) {
     console.log("you have no money...");
     document.getElementById("yourMoney").textContent = "Sorry, no money...";
     budget = 199;
-    setTimeout(renewBucks(), 2000)
-    setTimeout( alert("You got bonus 199$. Enjoy them!"), 2000);
+    setTimeout(renewBucks(), 2000);
+    setTimeout(alert("You got bonus 199$. Enjoy them!"), 2000);
     return;
   }
 
-  // console.log("function is done!");
+  console.log("function is done!");
   budget = budget - prices[`${drink}`];
-  
-  let cartNum  = parseInt(document.getElementById("cart").textContent);
-  cartNum = cartNum + 1
-  console.log(cartNum)
+
+  let cartNum = parseInt(document.getElementById("cart").textContent);
+  cartNum = cartNum + 1;
+  // console.log(cartNum)
   document.getElementById("cart").textContent = `${cartNum}`;
 
   console.log(budget);
   renewBucks();
 }
 
-
-renewBucks()
+renewBucks();
 
 renewDrink(20, "cokePrice");
 renewDrink(19, "fantaPrice");
@@ -77,16 +78,10 @@ renewDrink(9, "mojitoPrice");
 renewDrink(16, "fruitjuicesPrice");
 renewDrink(9, "lemonadePrice");
 
-
-
-
-
 // buyCoke.onclick = request("coke");
 // buyFanta.onclick = request("coke");
 // buyWater.onclick = request("coke");
 
-// buyCoke.addEventListener("click", request("coke"));
+// document.getElementsByClassName("coke").addEventListener("click", request("coke"));
 // buyFanta.addEventListener("click", request("fanta"));
 // buyWater.addEventListener("click", request("water"));
-
-
